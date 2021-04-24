29189f96026ecab4b59bb98b539f39197a0425aa
Aleksandr Movsesyan <aleksmovsesyan@gmail.com>
2021-04-23 19:28:43 -1000

    Adding code

---
+++ ge-cancellation-checker.phantom.js
@@ -0,0 +1,253 @@
+var system = require('system');
+var fs = require('fs');
+
+var schedule = false,
+    loadInProgress = false,
+    verbose = false,
+    curDate,
+    newDate,
+    python = false,
+    debug = false,
+    error = false,
+    earlierApptAvail = false;
+
+// XXX: send text/email notifications when rescheduling
+// XXX: accept arguments to automatically recheck
+// Calculate path of this file
+
+system.args.forEach(function(val, i) {
+    if (val == '-s' || val == '--schedule') { schedule = true; }
+    if (val == '-v' || val == '--verbose') { verbose = true; }
+    if (val == '-p' || val == '--python') { python = true; }
+});
+
+// Read settings from JSON
+try {
+    var settings = JSON.parse(fs.read(fs.absolute('config.json')));
+    if (!settings.logfile)
+        console.log('No logfile specified. Please specify logfile in' +
+                'config.json');
+    // Confirm JSON is loaded. Is this and python checks really necessary?
+    if (!settings.username || !settings.password || !settings.init_url || !settings.enrollment_location_id) {
+        console.log('Missing username, password, enrollment location ID, and/or initial URL. Exiting...');
+        phantom.exit();
+    }
+}
+catch(e) {
+    console.log(e + ' Could not find config.json');
+    phantom.exit();
+}
+
+// Set up log file
+try {
+    var logfile = fs.open(fs.absolute(settings.logfile), 'a');
+    phantom.aboutToExit.connect(logfile.flush);
+    phantom.aboutToExit.connect(logfile.close);
+    logfile.writeLine(Date().toString() + ' GOES APPOINTMENT CHECKER');
+    logfile.flush();
+}
+catch(e) {
+    console.log(e + ' Issue opening log file');
+}
+
+// Write to log
+function log(msg, pri) {
+    // XXX: pretty sure this will fail if log file doesn't open correctly
+    logfile.writeLine(Date().toString() + ' ' + msg);
+    if (verbose || pri) { console.log(msg); }
+    logfile.flush();
+}
+
+function pythonlog() {
+    if (python) {
+        console.log(curDate);
+        if (earlierApptAvail==true)
+            console.log("Earlier appt available");
+        else
+            console.log("Earlier appt unavailable");
+        console.log(newDate);
+    }
+}
+
+// Open and set up page
+var page = require('webpage').create();
+page.open(settings.init_url);
+page.onConsoleMessage = function(msg) {
+    log(msg);
+};
+
+page.onError = function(msg, trace) {
+    return;
+    console.error('Error on page: ' + msg);
+}
+
+page.onCallback = function(query, msg) {
+    if (query == 'username') { return settings.username; }
+    if (query == 'password') { return settings.password; }
+    if (query == 'enrollment_location_id') {
+        return settings.enrollment_location_id.toString(); }
+    if (query == 'schedule') {
+        return schedule; }
+    if (query == 'curDate') {
+        if (msg) { curDate = msg; return; }
+        else { return curDate; }
+    }
+    if (query == 'newDate') { newDate = msg; }
+    if (query == 'earlierApptAvail') {
+        if (msg) {
+            earlierApptAvail = true;
+            return; }
+        else {
+            return earlierApptAvail; }
+    }
+    if (query == 'fatal-error') {
+        log('Fatal error: ' + msg);
+        phantom.exit();
+    }
+    return null;
+}
+
+page.onLoadStarted = function() { loadInProgress = true; };
+page.onLoadFinished = function() { loadInProgress = false; };
+
+var steps = [
+    function() { // Login
+        page.evaluate(function() {
+            console.log('Logging in...');
+            try {
+                document.querySelector('input[name=j_username]').value =
+                window.callPhantom('username');
+                document.querySelector('input[name=j_password]').value =
+                window.callPhantom('password');
+                document.querySelector('input[name="Sign In"]').click();
+            }
+            catch (err) {
+                console.log(err);
+                error = true;
+            }
+        });
+    },
+    function() { // Accept terms of agreement
+        page.evaluate(function() {
+            console.log('Accepting terms...');
+            try {
+            document.querySelector('input[name=checkMe]').click();
+            }
+            catch (err) {
+                console.log(err);
+                error = true;
+            }
+        });
+    },
+    function() { // Appointment management button
+        page.evaluate(function() {
+            console.log('Entering appointment management...');
+            try {
+                document.querySelector('.bluebutton[name=manageAptm]').click();
+            }
+            catch (err) {
+                console.log(err);
+                error = true;
+            }
+        });
+    },
+    function() { // Collect current date
+        page.evaluate(function() {
+            // Current date XXX: clean up this search
+            console.log("Get the earliest reschedule date...");
+            try {
+            date = document.querySelector(".maincontainer p:nth-child(6)").innerHTML.replace(/<strong>[\s\S]*?<\/strong>/, "");
+            date += " " + document.querySelector(".maincontainer p:nth-child(7)").innerHTML.replace(/<strong>[\s\S]*?<\/strong>/, "");
+            console.log("date = " + date);
+            window.callPhantom('curDate', date);
+            console.log('Current date found: ' + date);
+            document.querySelector('input[name=reschedule]').click();
+            }
+            catch (err) {
+                console.log(err);
+                error = true;
+            }
+        });
+    },
+    function() { // Select enrollment center
+        page.evaluate(function() {
+                console.log('Selecting enrollment center ' + window.callPhantom('enrollment_location_id'));
+                var valToFind = window.callPhantom('enrollment_location_id');
+                try {
+                  var allInputs = document.getElementsByTagName("input");
+                  var results = [];
+                  for(var x=0;x<allInputs.length;x++) {
+                    if(allInputs[x].value == valToFind) {
+                      console.log('Found the input and clicking');
+                      allInputs[x].click();
+                      break;
+                    }
+                  }
+                  console.log('Clicking next');
+                  document.querySelector('input[name=next]').click();
+                  // document.querySelector('select[name=selectedEnrollmentCenter]').value = window.callPhantom('enrollment_location_id');
+                }
+                catch (err) {
+                    console.log(err);
+                    error = true;
+                }
+                });
+    },
+    function() { // Check next available appointment
+        page.evaluate(function() {
+            console.log('Checking for earlier appointment...');
+            // We made it! Now we have to scrape the page for the earliest available date
+            try {
+            var date = document.querySelector('.date table tr:first-child td:first-child').innerHTML;
+            var month_year = document.querySelector('.date table tr:last-child td:last-child div').innerHTML;
+            var newDate = month_year.replace(',', ' ' + date + ',');
+            newDate += " " + document.querySelector('a[href="#"].entry span').innerHTML;
+            console.log('Next date is ' + newDate);
+            window.callPhantom('newDate', newDate);
+            var curDate = window.callPhantom('curDate');
+            if(Date.parse(newDate).valueOf() < Date.parse(curDate).valueOf()) {
+                window.callPhantom('earlierApptAvail', true);
+                // XXX: format this for python script
+                console.log('Earlier appt available on ' + newDate);
+                var schedule = window.callPhantom('schedule');
+                if (schedule) {
+                    document.querySelector('a[href="#"].entry').onclick();
+                }
+            }}
+            catch (err) {
+                console.log(err);
+                error = true;
+            }
+        });
+    },
+    function() { // Confirm scheduling appointment
+        pythonlog();
+        if( earlierApptAvail && schedule ) {
+            page.evaluate( function() {
+                try {
+                console.log('Scheduling earlier appointment');
+                document.querySelector('input[name=comments]').value = "Found earlier appointment";
+                document.querySelector('input[name=Confirm]').click();
+                }
+                catch (err) {
+                    console.log(err);
+                    error = true;
+                }
+            });
+        }
+    } // XXX: confirm appointment scheduled. can't do this without losing my appointment
+];
+
+var i = 0;
+interval = setInterval(function() {
+    if (loadInProgress) { return; } // not ready yet...
+    if (typeof steps[i] != "function") {
+        return phantom.exit();
+    }
+    steps[i]();
+    if (error) {
+        phantom.exit();
+        clearInterval(interval);
+    }
+    i++;
+}, 100);
---
+++ ge-checker-cron.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+# Note: for setting up email with sendmail, see: http://linuxconfig.org/configuring-gmail-as-sendmail-email-relay
+
+from subprocess import check_output
+from datetime import datetime
+from os import getcwd
+import sys, smtplib, json
+from cStringIO import StringIO
+from getopt import getopt
+from argparse import ArgumentParser
+from time import sleep
+from random import random
+
+flags = {}
+
+def log(msg):
+    if flags.verbose:
+        print msg
+
+    if not 'logfile' in settings or not settings['logfile']: return
+    with open(settings['logfile'], 'a') as logfile:
+        logfile.write('%s: %s\n' % (datetime.now(), msg))
+
+def send_apt_available_email(current_apt, avail_apt, scheduled):
+    log('Attempting to send notification email...')
+    message = """From: %s
+To: %s
+Subject: Alert: New Global Entry Appointment Available
+Content-Type: text/html
+
+<p>Good news! There's a new Global Entry appointment available on <b>%s</b> (your current appointment is on %s).</p>
+
+<p>If this sounds good, please sign in to https://goes-app.cbp.dhs.gov/main/goes to reschedule.</p>
+""" % (settings['email_from'], ', '.join(settings['email_to']), avail_apt.strftime('%B %d, %Y'), current_apt.strftime('%B %d, %Y'))
+    if (scheduled):
+        message = """From: %s
+To: %s
+Subject: Alert: New Global Entry Appointment Scheduled
+Content-Type: text/html
+
+<p>Good news! An earlier Global Entry appointment is available and has been scheduled for you on <b>%s</b></p>
+
+<p>Your previous appointment was on %s.</p>
+
+""" % (settings['email_from'], ', '.join(settings['email_to']), avail_apt.strftime('%B %d, %Y %H:%M %p'), current_apt.strftime('%B %d, %Y'))
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(settings['email_from'], settings['gmail_pw'])
+        server.sendmail(settings['email_from'], settings['email_to'], message)
+        server.quit()
+    except Exception as e:
+        log(e)
+
+if __name__ == '__main__':
+    PWD = getcwd()
+    parser = ArgumentParser()
+    parser.add_argument('-s', '--schedule', action='store_true')
+    parser.add_argument('-r', '--repeat', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    flags = parser.parse_args()
+
+    # Get settings
+    try:
+        with open('%s/config.json' % PWD) as json_file:
+            settings = json.load(json_file)
+    except Exception as e:
+        print 'Error extracting config file: %s' % e
+
+    if not 'email_from' in settings or not settings['email_from']:
+        print 'Missing from address in config'
+        sys.exit()
+    if not 'email_to' in settings or not settings['email_to']:
+        print 'Missing to address in config'
+        sys.exit()
+
+    available = False
+    cmd = ['phantomjs', '%s/ge-cancellation-checker.phantom.js' % PWD, '-p'];
+    if (flags.schedule):
+        cmd.append('-s')
+    while True:
+        new_apt_str = check_output(cmd); # get string from PhantomJS script - formatted like 'Apr 7, 2017 10:00 AM\nEarlier appt available\nApril 2, 2017 07:45 PM'
+        aptStream = StringIO(new_apt_str)
+        curDate = datetime.strptime(aptStream.readline().strip(), '%b %d, %Y %H:%M %p')
+        available = (aptStream.readline().strip() == 'Earlier appt available')
+        newDate = datetime.strptime(aptStream.readline().strip(), '%B %d, %Y %H:%M %p')
+        new_apt_str = new_apt_str.strip()
+        aptStream.close()
+        if (not available):
+            log('No new appointments. Next available on %s (current is on %s)' % (newDate, curDate))
+            if (not flags.repeat):
+                sys.exit(0)
+            sleep_interval = settings['period'] * (1.5 - random())
+            log('Sleeping for %s minutes' % sleep_interval)
+            sleep(sleep_interval * 60)
+        else :
+            send_apt_available_email(curDate, newDate, flags.schedule)
